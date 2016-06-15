d5cfc502db175ff295f549b0008d3533a4d7c650
pearpages <pere@soms.cat>
2016-06-15 17:35:01 +0200

    systemjs

--- Module Loaders/systemjs/js/app.js
+++ Module Loaders/systemjs/js/app.js
@@ -1,5 +1,5 @@
-var Car = require('./models/Car');
-var Driver = require('./models/Driver');
+var Car = require('./models/Car.js');
+var Driver = require('./models/Driver.js');
 
 var car1 = new Car('Ferrari', 'dkd35123');
 var car2 = new Car('Lamborghini', '1234XYZ');
